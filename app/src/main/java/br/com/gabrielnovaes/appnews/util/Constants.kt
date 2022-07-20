3e3b4195f5e7332639cb2253a41515d43747f3d1
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-07-20 20:28:23 -0300

    added network instance

---
+++ app/src/main/java/br/com/gabrielnovaes/appnews/util/Constants.kt
@@ -0,0 +1,10 @@
+package br.com.gabrielnovaes.appnews.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "a995c8de5fde46dda8ab8e47d49b5d6e"
+        const val BASE_URL = "https://newsapi.org"
+    }
+
+}
